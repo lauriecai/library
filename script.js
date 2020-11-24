d3c04b5d5d63876fbf8f30c6aa5c81e6286deda4
Laurie Cai <laurie801@gmail.com>
2020-11-24 13:30:21 -0800

    Add createCard() function

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
+const book0 = new book('The Life-Changing Magic of Tidying Up', 'Marie Kondo', '226 Pages');
+const book1 = new book('Harry Potter and the Chamber of Secrets','J.K. Rowling', '341 Pages')
+
+myLibrary.push(book0, book1);
+
+function book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -15,6 +20,38 @@ function Book(title, author, pages, read) {
     }
 }
 
+// selectors
+const cardGroup = document.querySelector('.card-group');
+const card = document.querySelector('.card');
+
+// functions
+function createCard() {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    cardGroup.appendChild(card);
+}
+
 function addBookToLibrary() {
-    // do stuff here
+    for (i = 0; i < myLibrary.length; i++) {
+
+        // create 'card' div
+        createCard();
+        // create 'title', 'author', and 'page-count' children elements
+        const title = document.createElement('p');
+        title.classList.add('title');
+        card.appendChild(title);
+
+        const author = document.createElement('p');
+        title.classList.add('author');
+        card.appendChild(author);
+
+        const pageCount = document.createElement('p');
+        title.classList.add('page-count');
+        card.appendChild(pageCount);
+
+        // assign title, author, and page count
+        // document.querySelector('.title').textContent = book[i].title;
+        // document.querySelector('.author').textContent = book[i].author;
+        // document.querySelector('.title').textContent = book[i].pageCount;
+    }
 }
