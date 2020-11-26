8ec1d83bdf891fe9084846f87598281d113243bb
Laurie Cai <laurie801@gmail.com>
2020-11-26 11:18:48 -0800

    Add title input autofocus

--- script.js
+++ script.js
@@ -5,22 +5,13 @@ const cards = document.getElementsByClassName('card');
 const title = document.getElementsByClassName('title');
 const author = document.getElementsByClassName('author');
 const pages = document.getElementsByClassName('pages');
-const addBookBtn = document.querySelector('#add-btn-1');
+const addBookBtns = document.querySelectorAll('.add-btn');
 const modal = document.querySelector('.dark-overlay');
 const modalTitle = document.querySelector('.titleInput');
 const modalAuthor = document.querySelector('.authorInput');
 const modalPages = document.querySelector('.pagesInput');
 const modalCancel = document.querySelector('.cancel-btn');
-const modalAdd = document.querySelector('#add-btn-2');
-
-// -----OBJECTS-----
-// test books
-// const book0 = new book('The Life-Changing Magic of Tidying Up', 'Marie Kondo', '226 Pages');
-// const book1 = new book('Harry Potter and the Chamber of Secrets','J.K. Rowling', '341 Pages')
-// const book2 = new book('The Making of a Manager', 'Julie Zhuo', '288 Pages')
-// const book3 = new book('Becoming', 'Michelle Obama', '448 Pages')
-// const book4 = new book('A Promised Land', 'Barack Obama', '768 Pages')
-// myLibrary.push(book0, book1, book2, book3, book4);
+const modalAdd = document.querySelector('.add-confirm-btn');
 
 // -----FUNCTIONS-----
 // object constructor
@@ -41,6 +32,8 @@ function book(title, author, pages, read) {
 
 // collect book info
 function addBook() {
+    // create card
+    createCard();
     // create new bookvar: let 'book' + `${myLibrary.length}`
     let bookvar = 'book' + `${myLibrary.length}`;
     bookvar = new book(`${modalTitle.value}`, `${modalAuthor.value}`, `${modalPages.value}`);
@@ -48,6 +41,13 @@ function addBook() {
     myLibrary.push(bookvar);
     // generate updated library
     generateLibrary();
+    // close modal
+    hide('.dark-overlay');
+    hide('.null-state')
+    // reset values
+    modalTitle.value = '';
+    modalAuthor.value = '';
+    modalPages.value = ''
 }
 
 // create card
@@ -90,17 +90,19 @@ function show(classname) {
 // generate library
 function generateLibrary() {
     for (i = 0; i < myLibrary.length; i++) {
-        createCard();
         populateBookInfo(i);
         show('.button-top-right');
     }
 }
 
 // -----FLOW-----
-// user clicks add book > show modal
-addBookBtn.addEventListener('click', function() {
-    modal.style.display = 'block';
-})
+// add event listeners to all add book buttons
+for (i = 0; i < addBookBtns.length; i++) {
+    addBookBtns[i].addEventListener('click', function() {
+        modal.style.display = 'block';
+        modalTitle.focus();
+    })
+}
 
 // modal > user clicks 'cancel'
 modalCancel.addEventListener('click', function() {
@@ -109,5 +111,5 @@ modalCancel.addEventListener('click', function() {
 
 // modal > user clicks 'add book'
 modalAdd.addEventListener('click', function() {
-    collectBookInfo();
+    addBook();
 })
