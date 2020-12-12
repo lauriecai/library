83f1153f7e608e9729774e350ac988c68135c995
Laurie Cai <laurie801@gmail.com>
2020-12-12 12:14:59 -0800

    Create addBook() function

--- app.js
+++ app.js
@@ -7,25 +7,74 @@ class Book {
     }
 }
 
+// UI class - represents the user interface
+class UI {
+    static displayAddModal() {
+        document.querySelector('.dark-overlay').style.display = 'block';
+    }
 
+    static addBook() {
+        // grab inputted values
+        const titleValue = document.querySelector('.titleInput').value;
+        const authorValue = document.querySelector('.authorInput').value;
+        const pagesValue = document.querySelector('.pagesInput').value;
+        console.log({titleValue, authorValue, pagesValue});
 
-// class User {
-//     constructor(name, email) {
-//         this.name = name;
-//         this.email = email;
-//         this.patients = 0;
-//     }
-//     login() {
-//         console.log(this.email + ' just logged in');
-//         return this;
-//     }
-//     logout() {
-//         console.log(this.email + ' just logged out');
-//         return this;
-//     }
-//     updatePatients() {
-//         this.patients++;
-//         console.log(this.name + ' has had ' + this.patients + ' patients.');
-//         return this;
-//     }
-// }
+        // create book card
+        const card = document.createElement('div');
+        card.className = 'card';
+
+        // create status, title, author, and pages
+        const status = document.createElement('div');
+        status.className = 'status';
+            const input = document.createElement('input');
+            input.type = 'checkbox';
+            input.className = 'checkbox';
+            const label = document.createElement('label');
+            label.textContent = "I've Read This";
+        status.appendChild(input);
+        status.appendChild(label);
+        const title = document.createElement('p');
+        title.className = 'title';
+        const author = document.createElement('p');
+        author.className = 'author';
+        const footer = document.createElement('div');
+        footer.className = 'book-footer';
+            const pages = document.createElement('p');
+            pages.className = 'pages';
+            const remove = document.createElement('p');
+            remove.className = 'remove';
+        footer.appendChild(pages);
+        footer.appendChild(remove);
+
+        // append card children to card
+        card.appendChild(status);
+        card.appendChild(title);
+        card.appendChild(author);
+        card.appendChild(footer);
+
+        // append card to card group
+        const cardGroup = document.querySelector('.card-group');
+        cardGroup.appendChild(card);
+        // hide null state
+        document.querySelector('.null-state').style.display = 'none';
+        // close modal
+        document.querySelector('.dark-overlay').style.display = 'none';
+
+        // populate book info
+        document.querySelector('.title').textContent = titleValue;
+        document.querySelector('.author').textContent = authorValue;
+        document.querySelector('.pages').textContent = pagesValue;
+        document.querySelector('.remove').textContent = 'Remove';
+    }
+}
+
+// event listeners
+
+// add book button > invokes add book modal
+Array.from(document.getElementsByClassName('add-btn')).forEach(element => {
+    element.addEventListener('click', UI.displayAddModal);
+})
+
+// add book button (modal) > confirms book add
+document.getElementById('add-btn-modal').addEventListener('click', UI.addBook);
